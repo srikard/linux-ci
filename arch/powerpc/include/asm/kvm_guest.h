2cfa0f80ac97cc18061bdf5c1bd488fc0c8d462d
Srikar Dronamraju <srikar@linux.vnet.ibm.com>
2020-09-23 08:11:08 +0530

    powerpc: Reintroduce is_kvm_guest in a new avatar
    Introduce a static branch that would be set during boot if the OS
    happens to be a KVM guest. Subsequent checks to see if we are on KVM
    will rely on this static branch. This static branch would be used in
    vcpu_is_preempted in a subsequent patch.
    
    Cc: linuxppc-dev <linuxppc-dev@lists.ozlabs.org>
    Cc: LKML <linux-kernel@vger.kernel.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Nathan Lynch <nathanl@linux.ibm.com>
    Cc: Gautham R Shenoy <ego@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Valentin Schneider <valentin.schneider@arm.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Phil Auld <pauld@redhat.com>
    Acked-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>

--- arch/powerpc/include/asm/kvm_guest.h
+++ arch/powerpc/include/asm/kvm_guest.h
@@ -7,8 +7,18 @@
 #define __POWERPC_KVM_GUEST_H__
 
 #if defined(CONFIG_PPC_PSERIES) || defined(CONFIG_KVM_GUEST)
+#include <linux/jump_label.h>
+
+DECLARE_STATIC_KEY_FALSE(kvm_guest);
+
+static inline bool is_kvm_guest(void)
+{
+	return static_branch_unlikely(&kvm_guest);
+}
+
 bool check_kvm_guest(void);
 #else
+static inline bool is_kvm_guest(void) { return false; }
 static inline bool check_kvm_guest(void) { return false; }
 #endif
 
