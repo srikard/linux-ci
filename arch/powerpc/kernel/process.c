51c374ad78934bbcb4dc526c14b695311895722b
Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
2020-11-25 10:46:23 +0530

    powerpc/book3s64/pkeys: Inherit correctly on fork.
    Child thread.kuap value is inherited from the parent in
    copy_thread_tls. We still need to make sure when the child returns
    from a fork in the kernel we start with the kernel default AMR value.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Reviewed-by: Sandipan Das <sandipan@linux.ibm.com>
    [mpe: Incorporate fixup patch from Aneesh]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20201125051634.509286-12-aneesh.kumar@linux.ibm.com

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -1769,6 +1769,16 @@ int copy_thread(unsigned long clone_flags, unsigned long usp,
 
 	p->thread.tidr = 0;
 #endif
+	/*
+	 * Run with the current AMR value of the kernel
+	 */
+#ifdef CONFIG_PPC_PKEY
+	if (mmu_has_feature(MMU_FTR_KUAP))
+		kregs->amr = AMR_KUAP_BLOCKED;
+
+	if (mmu_has_feature(MMU_FTR_KUEP))
+		kregs->iamr = AMR_KUEP_BLOCKED;
+#endif
 	kregs->nip = ppc_function_entry(f);
 	return 0;
 }
